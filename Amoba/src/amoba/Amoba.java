0db88027f7257daf3bb3091d8ffe000def999f7b
David <lkcsdvd@hotmail.com>
2020-11-08 13:13:05 +0100

    doneeeeeeeeeeeeeeeeeeeeeeeeee

--- Amoba/src/amoba/Amoba.java
+++ Amoba/src/amoba/Amoba.java
@@ -122,6 +122,8 @@ public class Amoba {
         
         XareasInARow=0;
         OareasInARow=0;
+        varjmegO =  true;
+        varjmegX =  true;
         
         System.out.println("Oszlop check");
         // oszlop check
@@ -131,86 +133,34 @@ public class Amoba {
                 {
                     if(fieldButtons.get(idx).getText().equals("X"))
                     {
+                        if(OareasInARow==3)
+                        {
+                            varjmegO=false;
+                        }
                         OareasInARow=0;
                         ++XareasInARow;
                         System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                        //System.out.println(areasInARow);
                     }
                     else
                     {
+                        if(XareasInARow==3)
+                        {
+                            varjmegX=false;
+                        }
                         XareasInARow=0;
                         ++OareasInARow;
                         System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
-                    }                          
-                }
-                else
-                    {
-                        XareasInARow=0;
-                        OareasInARow=0;
+                        //System.out.println(areasInARow);
                     }
-            if(XareasInARow==5)
-            {
-                System.out.println("GG");
-                winner=playerX;                         
-            }
-            else if(OareasInARow==5)
-            {
-                System.out.println("GGO");
-                    winner= playerO;
-            }
-            else if(XareasInARow==4 &&!gotPranked &&  !Player.getXTurn() )
-            {
-                playerX.prankPlayer().prankPlayer();
-                
-                gotPranked = true;
-                System.out.println("pranked3");
-            }
-            else if(OareasInARow==4 && !gotPranked && Player.getXTurn()  )
-            {
-                playerO.prankPlayer().prankPlayer();
-               
-                gotPranked = true;
-                System.out.println("pranked4");
-            } 
-            else if(XareasInARow==3 &&!gotPranked &&  !Player.getXTurn() )
-            {
-                playerX.prankPlayer();
-                gotPranked = true;
-                System.out.println("pranked1");
-            }
-            else if(OareasInARow==3 &&!gotPranked &&  Player.getXTurn() )
-            {
-                playerO.prankPlayer();
-                gotPranked = true;
-                System.out.println("pranked2");
-            }
-            }
-            
-            XareasInARow=0;
-            OareasInARow=0;
-        System.out.println("Jobbátló check");
-        //átló check jobbra
-        for(int idx=it.getCoordX()>it.getCoordY() ? it.getAtloFirstElem() * (int)Math.sqrt(fieldButtons.size()) : it.getAtloFirstElem(); idx<fieldButtons.size(); idx+=Math.sqrt(fieldButtons.size())+1)
-            {
-              if(!(fieldButtons.get(idx).getText().equals("")))
-                {
-                    if(fieldButtons.get(idx).getText().equals("X"))
-                    {
-                        OareasInARow=0;
-                        ++XareasInARow;
-                        System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
-                    }
-                    else
-                    {
-                        XareasInARow=0;
-                        ++OareasInARow;
-                         System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
-                    }       
+    
                 }
                 else
                 {
                     XareasInARow=0;
                     OareasInARow=0;
                 }
+                
                 if(XareasInARow==5)
                 {
                     System.out.println("GG");
@@ -219,89 +169,228 @@ public class Amoba {
                 else if(OareasInARow==5)
                 {
                     System.out.println("GGO");
-                    winner=playerO;
+                    winner= playerO;
                 }
-                else if(XareasInARow==4 && !gotPranked && !Player.getXTurn())
+                else if(XareasInARow==4 &&!gotPranked &&  !Player.getXTurn() )
                 {
                     playerX.prankPlayer().prankPlayer();
+                    
                     gotPranked = true;
-                    System.out.println("pranked7");
+                    System.out.println("pranked3");
                 }
-                else if(OareasInARow==4 &&!gotPranked &&  Player.getXTurn() )
+                else if(OareasInARow==4 && !gotPranked && Player.getXTurn()  )
                 {
                     playerO.prankPlayer().prankPlayer();
-                    System.out.println("pranked8");
+                   
+                    gotPranked = true;
+                    System.out.println("pranked4");
                 } 
-                else if(XareasInARow==3  && !gotPranked && !Player.getXTurn()  )
+                else if(XareasInARow==3 &&!gotPranked &&  !Player.getXTurn() && varjmegX || !varjmegX   )
                 {
+                    if(varjmegX)
+                    {
+                        continue;
+                    }
                     playerX.prankPlayer();
                     gotPranked = true;
-                    System.out.println("pranked5");
+                    System.out.println("pranked1");
+                    varjmegX=true;
+                    
                 }
-                else if(OareasInARow==3  && !gotPranked && Player.getXTurn() )
+                else if(OareasInARow==3 &&!gotPranked &&  Player.getXTurn() && varjmegO || !varjmegO )
                 {
+                    if(varjmegO)
+                    {
+                        continue;
+                    }
                     playerO.prankPlayer();
                     gotPranked = true;
-                    System.out.println("pranked6");
+                    System.out.println("pranked2");
+                    varjmegO=true;
+    
+                    
                 }
-            }  
+            }
+            
+            XareasInARow=0;
+            OareasInARow=0;
+            varjmegO =  true;
+            varjmegX =  true;
+        System.out.println("Jobbátló check");
+        //átló check jobbra
+        for(int idx=it.getCoordX()>it.getCoordY() ? it.getAtloFirstElem() * (int)Math.sqrt(fieldButtons.size()) : it.getAtloFirstElem(); idx<fieldButtons.size(); idx+=Math.sqrt(fieldButtons.size())+1)
+            {
+                if(!(fieldButtons.get(idx).getText().equals("")))
+                {
+                    if(fieldButtons.get(idx).getText().equals("X"))
+                    {
+                        if(OareasInARow==3)
+                        {
+                            varjmegO=false;
+                        }
+                        OareasInARow=0;
+                        ++XareasInARow;
+                        System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                        //System.out.println(areasInARow);
+                    }
+                    else
+                    {
+                        if(XareasInARow==3)
+                        {
+                            varjmegX=false;
+                        }
+                        XareasInARow=0;
+                        ++OareasInARow;
+                        System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                        //System.out.println(areasInARow);
+                    }
+    
+                }
+                else
+                {
+                    XareasInARow=0;
+                    OareasInARow=0;
+                }
+                
+                if(XareasInARow==5)
+                {
+                    System.out.println("GG");
+                    winner=playerX;                         
+                }
+                else if(OareasInARow==5)
+                {
+                    System.out.println("GGO");
+                    winner= playerO;
+                }
+                else if(XareasInARow==4 &&!gotPranked &&  !Player.getXTurn() )
+                {
+                    playerX.prankPlayer().prankPlayer();
+                    
+                    gotPranked = true;
+                    System.out.println("pranked3");
+                }
+                else if(OareasInARow==4 && !gotPranked && Player.getXTurn()  )
+                {
+                    playerO.prankPlayer().prankPlayer();
+                   
+                    gotPranked = true;
+                    System.out.println("pranked4");
+                } 
+                else if(XareasInARow==3 &&!gotPranked &&  !Player.getXTurn() && varjmegX || !varjmegX   )
+                {
+                    if(varjmegX)
+                    {
+                        continue;
+                    }
+                    playerX.prankPlayer();
+                    gotPranked = true;
+                    System.out.println("pranked1");
+                    varjmegX=true;
+                    
+                }
+                else if(OareasInARow==3 &&!gotPranked &&  Player.getXTurn() && varjmegO || !varjmegO )
+                {
+                    if(varjmegO)
+                    {
+                        continue;
+                    }
+                    playerO.prankPlayer();
+                    gotPranked = true;
+                    System.out.println("pranked2");
+                    varjmegO=true;
+    
+                    
+                }
+            } 
+            
+            XareasInARow=0;
+            OareasInARow=0;
+            varjmegO =  true;
+            varjmegX =  true;
             System.out.println("Balátló check");
             //System.out.println( it.getCoordX()>it.getCoordY() ? it.getAtloFirstElem() * (int)Math.sqrt(fieldButtons.size()) : it.getAtloFirstElem()  );
             for(int idx = it.getAtloFirstElem2((int)Math.sqrt(fieldButtons.size())); idx<fieldButtons.size(); idx+=Math.sqrt(fieldButtons.size())-1)
             {
-            if(!(fieldButtons.get(idx).getText().equals("")))
+                if(!(fieldButtons.get(idx).getText().equals("")))
                 {
-                if(fieldButtons.get(idx).getText().equals("X"))
+                    if(fieldButtons.get(idx).getText().equals("X"))
+                    {
+                        if(OareasInARow==3)
                         {
-                            OareasInARow=0;
-                            ++XareasInARow;
-                            System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                            varjmegO=false;
                         }
-                else
+                        OareasInARow=0;
+                        ++XareasInARow;
+                        System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                        //System.out.println(areasInARow);
+                    }
+                    else
+                    {
+                        if(XareasInARow==3)
                         {
-                            XareasInARow=0;
-                            ++OareasInARow;
-                            System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
-                        }       
+                            varjmegX=false;
+                        }
+                        XareasInARow=0;
+                        ++OareasInARow;
+                        System.out.println("Oareas: " + OareasInARow + " Xareas:" + XareasInARow);
+                        //System.out.println(areasInARow);
+                    }
+    
                 }
                 else
-                    {
-                        XareasInARow=0;
-                        OareasInARow=0;
-                    }
+                {
+                    XareasInARow=0;
+                    OareasInARow=0;
+                }
+                
                 if(XareasInARow==5)
-                    {
-                        System.out.println("GG");
-                        winner=playerX;                         
-                    }
+                {
+                    System.out.println("GG");
+                    winner=playerX;                         
+                }
                 else if(OareasInARow==5)
-                    {
-                        System.out.println("GGO");
-                        winner=playerO;
-                    } 
-                else if(XareasInARow==4 &&!gotPranked &&  !Player.getXTurn())
+                {
+                    System.out.println("GGO");
+                    winner= playerO;
+                }
+                else if(XareasInARow==4 &&!gotPranked &&  !Player.getXTurn() )
                 {
                     playerX.prankPlayer().prankPlayer();
+                    
                     gotPranked = true;
-                    System.out.println("pranked11");
+                    System.out.println("pranked3");
                 }
-                else if(OareasInARow==4 && !gotPranked && Player.getXTurn() )
+                else if(OareasInARow==4 && !gotPranked && Player.getXTurn()  )
                 {
                     playerO.prankPlayer().prankPlayer();
+                   
                     gotPranked = true;
-                    System.out.println("pranked12");
-                }
-                else if( XareasInARow==3 && !gotPranked && !Player.getXTurn())
+                    System.out.println("pranked4");
+                } 
+                else if(XareasInARow==3 &&!gotPranked &&  !Player.getXTurn() && varjmegX || !varjmegX   )
                 {
+                    if(varjmegX)
+                    {
+                        continue;
+                    }
                     playerX.prankPlayer();
                     gotPranked = true;
-                    System.out.println("pranked9");
+                    System.out.println("pranked1");
+                    varjmegX=true;
+                    
                 }
-                else if(OareasInARow==3 && !gotPranked && Player.getXTurn())
+                else if(OareasInARow==3 &&!gotPranked &&  Player.getXTurn() && varjmegO || !varjmegO )
                 {
+                    if(varjmegO)
+                    {
+                        continue;
+                    }
                     playerO.prankPlayer();
                     gotPranked = true;
-                    System.out.println("pranked10");
+                    System.out.println("pranked2");
+                    varjmegO=true;
+    
+                    
                 }   
             }
             return;
